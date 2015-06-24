fbf3b2c975733de99e4e9feb77d50df07f97e989
Steven Cothren <stevencothren2005@gmail.com>
2015-06-23 21:15:59 -0500

    Added very basic Player 1 AI.
    Added basic functionality for checking player moves and applying them. Needs to be refactored and some edge cases need to be added to the logic.

--- player_1.py
+++ player_1.py
@@ -3,7 +3,35 @@
 def player_ai(game_board, player_hand, first_move):
     print("Player 1 AI Called")
 
-    if first_move:
-        print("In First Move")
+    moves = []
 
-    return []
+    if first_move:
+        hand = [[None for x in range(13)] for x in range(4)]
+        for tile_id in player_hand:
+            tile = player_hand[tile_id]
+            if tile['suit'] == "Black":
+                hand[0][tile['value']-1] = tile
+            elif tile['suit'] == "Blue":
+                hand[1][tile['value']-1] = tile
+            elif tile['suit'] == "Red":
+                hand[2][tile['value']-1] = tile
+            elif tile['suit'] == "Yellow":
+                hand[3][tile['value']-1] = tile
+
+        possible_groups = []
+        for i in range(13):
+            total = 0
+            for j in range(4):
+                if hand[j][i]:
+                    total += 1
+            possible_groups.append(total)
+
+        for i, val in enumerate(possible_groups):
+            if val >= 3:
+                new_move = []
+                for j in range(4):
+                    if hand[j][i]:
+                        new_move.append(hand[j][i]['id'])
+                moves.append(new_move)
+
+    return moves
--- rummy_cubes.py
+++ rummy_cubes.py
@@ -14,22 +14,36 @@ class RummyCubesGame:
 
     def run(self):
         while not self.game_over:
-            for player in self.players:
+            for player_idx, player in enumerate(self.players):
                 if self.game_over:
                     break
 
-                player_actions = player.move(deepcopy(self.board))
+                player_name = "Player " + str(player_idx + 1)
+
+                print(player_name + "'s turn")
+                player.print_hand()
+                player_moves = player.move(deepcopy(self.board))
 
                 # Player gave no actions, so is drawing a tile
-                if not player_actions:
+                if not player_moves:
                     player.add_tile(self.draw_tile())
-                    print("Draw Tile")
+                    print("\tDrew a Tile")
+                    player.print_hand()
                 else:
-                    if self.process_move(player, player_actions):
-                        print("Process Move")
+                    print("Processing Player " + player_name + " Moves")
+                    if self.process_move(player_idx, player_moves):
+                        # TODO: Fix this nastiness!
+                        player = self.players[player_idx]
+                        print("\tMoves Successful")
                     else:
                         player.add_tile(self.draw_tile())
-                        print("Invalid Player Actions - Draw Tile")
+                        print("\tInvalid Player Actions - Draw Tile")
+
+                    player.print_hand()
+
+                if len(player.get_player_hand()) == 0:
+                    self.winner = player_idx + 1
+                    self.game_over = True
 
             self.game_over = True
             self.winner = 1
@@ -80,15 +94,96 @@ class RummyCubesGame:
     def draw_tile(self):
         return self.bag.pop()
 
-    def process_move(self, player, player_actions):
-        # Validate player actions
+    def process_move(self, player_idx, player_moves):
+        new_player = deepcopy(self.players[player_idx])
+        new_board = deepcopy(self.board)
+        new_player_hand = new_player.get_player_hand()
+        valid_move = True
 
-        # If legal
-        # Update player hand based on actions
-        # Update player first_move
-        # Update board based on actions
-        # Check for game won
-        return True
+        print("Player moves : ")
+        for move in player_moves:
+            if not valid_move:
+                break
+
+            new_tile_set = []
+            for p_id in move:
+                print("\t" + str(p_id))
+                if new_player_hand[p_id]:
+                    new_tile_set.append(new_player_hand[p_id])
+                    new_player.remove_tile(p_id)
+                else:
+                    on_board = False
+
+                    for i, tile_set in enumerate(new_board):
+                        for j, tile in enumerate(tile_set):
+                            if tile['id'] == p_id:
+                                on_board = True
+                                new_tile_set.append(tile)
+                                del new_board[i][j]
+                                break
+                        if on_board:
+                            break
+
+                    if not on_board:
+                        valid_move = False
+                        break
+            if valid_move:
+                new_board.append(new_tile_set)
+
+        for i, tile_set in enumerate(new_board):
+            if not valid_move:
+                break
+
+            if len(tile_set) == 0:
+                del new_board[i]
+            elif len(tile_set) < 3:
+                valid_move = False
+            else:
+                valid_set = True
+                valid_run = True
+
+                set_value = -1
+                run_values = []
+                tile_suits = {}
+
+                for tile in tile_set:
+                    if set_value == -1:
+                        set_value = tile['value']
+                    elif tile['value'] != set_value:
+                        valid_set = False
+
+                    if tile['suit'] in tile_suits:
+                        valid_set = False
+                    else:
+                        tile_suits[tile['suit']] = 1
+
+                    run_values.append(tile['value'])
+
+                if not valid_set:
+                    if len(tile_suits) > 1:
+                        valid_run = False
+                    else:
+                        sorted_values = sorted(run_values)
+                        cur_val = -1
+                        for value in sorted_values:
+                            if cur_val == -1:
+                                cur_val = value
+                            elif value == cur_val + 1:
+                                cur_val = value
+                            else:
+                                valid_run = False
+                                break
+
+                if not valid_set and not valid_run:
+                    valid_move = False
+
+        print("Valid: " + str(valid_move))
+
+        if valid_move:
+            self.players[player_idx] = new_player
+            self.board = new_board
+
+        return valid_move
 
 
 class RummyCubesPlayer:
@@ -108,6 +203,12 @@ class RummyCubesPlayer:
 
     def add_tile(self, tile):
         self.hand[tile['id']] = tile
+
+    def print_hand(self):
+        print("Player Hand: ")
+        for tile_id in self.hand:
+            print("\t" + str(self.hand[tile_id]['id']) + " : " +
+                  self.hand[tile_id]['suit'] + " : " + str(self.hand[tile_id]['value']))
 
 
 def main(num_players, num_games):
